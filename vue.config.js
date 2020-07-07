fd6614101648e05aad2f61aa4cad3649255df3f5
chenyulin <chenyulin@qiyi.com>
2020-07-07 10:32:39 +0800

    feat: 视口 vw/vh 适配

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+const autoprefixer = require('autoprefixer')
+const pxtoviewport = require('postcss-px-to-viewport')
+
 module.exports = {
   lintOnSave: true,
-  productionSourceMap: false // 去除生成环境.map文件
+  productionSourceMap: false, // 去除生成环境.map文件
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          autoprefixer(),
+          pxtoviewport({
+            viewportWidth: 375,
+            // 该项仅在使用 Circle 组件时需要
+            // 原因参见 https://github.com/youzan/vant/issues/1948
+            selectorBlackList: ['van-circle__layer']
+          })
+        ]
+      }
+    }
+  }
 }
