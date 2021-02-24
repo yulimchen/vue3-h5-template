37ca2473f5b13bef8e074d294eb070932ad11929
yulimchen <yulimchen@gmail.com>
2021-02-24 16:20:00 +0800

    chore(*): 修改vue.config.js打包资源目录

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,6 @@ function resolve(dir) {
 
 module.exports = {
   lintOnSave: true,
-  outputDir: 'docs',
-  publicPath: './',
   productionSourceMap: false, // 去除生成环境.map文件
   configureWebpack: {
     resolve: {
