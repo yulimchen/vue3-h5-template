e184a6e330f596b72adfd10da0f29585c1706869
yulimchen <yulimchen@gmail.com>
2021-02-25 17:34:13 +0800

    feat(*): 根据路由动态设置页面title

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 import routes from './routes'
 import store from '../store'
+import setPageTitle from '@/utils/set-page-title.js'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -8,7 +9,10 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  // 路由缓存
   store.commit('cachedView/ADD_CACHED_VIEW', to)
+  // 设置页面 title
+  setPageTitle(to.meta.title)
   next()
 })
 
