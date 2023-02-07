9be036d84e6713ffcf4fc2c4b4991f0a4b3e86e3
yulimchen <yulimchen@gmail.com>
2023-02-07 18:10:45 +0800

    feat: 按环境启用 `eruda` 调试工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import Components from "unplugin-vue-components/vite";
@@ -9,50 +9,63 @@ import path from "path";
 import mockDevServerPlugin from "vite-plugin-mock-dev-server";
 import vueSetupExtend from "vite-plugin-vue-setup-extend";
 import viteCompression from "vite-plugin-compression";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    mockDevServerPlugin(),
-    // vant 组件自动按需引入
-    Components({
-      resolvers: [VantResolver()]
-    }),
-    // svg icon
-    createSvgIconsPlugin({
-      // 指定图标文件夹
-      iconDirs: [path.resolve(process.cwd(), "src/icons/svg")],
-      // 指定 symbolId 格式
-      symbolId: "icon-[dir]-[name]"
-    }),
-    // 允许 setup 语法糖上添加组件名属性
-    vueSetupExtend(),
-    // 生产环境 gzip 压缩资源
-    viteCompression()
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
-    }
-  },
-  server: {
-    // 仅在 proxy 中配置的代理前缀， mock-dev-server 才会拦截并 mock
-    // doc: https://github.com/pengzhanbo/vite-plugin-mock-dev-server
-    proxy: {
-      "^/dev-api": {
-        target: ""
+export default defineConfig(({ mode }) => {
+  // 环境变量
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      mockDevServerPlugin(),
+      // vant 组件自动按需引入
+      Components({
+        resolvers: [VantResolver()]
+      }),
+      // svg icon
+      createSvgIconsPlugin({
+        // 指定图标文件夹
+        iconDirs: [path.resolve(process.cwd(), "src/icons/svg")],
+        // 指定 symbolId 格式
+        symbolId: "icon-[dir]-[name]"
+      }),
+      // 允许 setup 语法糖上添加组件名属性
+      vueSetupExtend(),
+      // 生产环境 gzip 压缩资源
+      viteCompression(),
+      // 注入模板数据
+      createHtmlPlugin({
+        inject: {
+          data: {
+            ENABLE_ERUDA: env.VITE_ENABLE_ERUDA || "false"
+          }
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
+    },
+    server: {
+      // 仅在 proxy 中配置的代理前缀， mock-dev-server 才会拦截并 mock
+      // doc: https://github.com/pengzhanbo/vite-plugin-mock-dev-server
+      proxy: {
+        "^/dev-api": {
+          target: ""
+        }
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]"
+        }
       }
     }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: "static/js/[name]-[hash].js",
-        entryFileNames: "static/js/[name]-[hash].js",
-        assetFileNames: "static/[ext]/[name]-[hash].[ext]"
-      }
-    }
-  }
+  };
 });
