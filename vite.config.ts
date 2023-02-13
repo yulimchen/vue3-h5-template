b86c706f6e63646f3576b3749a663d5ad9d7277b
yulimchen <yulimchen@gmail.com>
2023-02-13 11:47:55 +0800

    chore: 调整开发 `server` 配置

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     server: {
+      host: true,
       // 仅在 proxy 中配置的代理前缀， mock-dev-server 才会拦截并 mock
       // doc: https://github.com/pengzhanbo/vite-plugin-mock-dev-server
       proxy: {
