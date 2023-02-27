84da300e107fdfe5870ccd7f05715a022a952140
wswmsword <cjzjzzz@gmail.com>
2023-02-27 20:33:43 +0800

    chore: 更新屏幕适配插件，修复 tabbar 宽度问题

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   plugins: {
     // 使用 postcss-mobile-to-multi-displays 做 vw 适配，限制最大宽度，兼容桌面端
-    "postcss-mobile-to-multi-displays": {
-      rootClass: "app-wrapper", // 视图的根选择器名称
+    "postcss-mobile-forever": {
+      rootSelector: "#app", // 视图的根选择器名称
       viewportWidth: 375, // 根据设计稿设定
-      unitPrecision: 2 // 转化精度，转换后保留位数
+      unitPrecision: 2, // 转化精度，转换后保留位数
+      border: true, // 桌面和横屏视图的边框
+      desktopWidth: 600, // 桌面端视图宽度
+      landscapeWidth: 425, // 横屏视图宽度
+      disableDesktop: false, // 关闭桌面端适配
+      disableLandscape: false // 关闭横屏适配
     },
     autoprefixer: {
       overrideBrowserslist: ["Android >= 4.0", "iOS >= 7"]
