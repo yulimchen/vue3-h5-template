ee9d06042b48115f0c342e180376005ae2ed3569
yulimchen <yulimchen@gmail.com>
2023-11-06 23:58:38 +0800

    chore: 加入eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,15 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier"
   ],
   parserOptions: {
     ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
     sourceType: "module",
     jsxPragma: "React",
     ecmaFeatures: {
