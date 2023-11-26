ae0c5b0c19a0f33231b46d161642f251656583f3
yulimchen <yulimchen@gmail.com>
2023-11-26 15:41:06 +0800

    chore: 生产环境加载 `eruda`

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import path from "path";
 import mockDevServerPlugin from "vite-plugin-mock-dev-server";
 import viteCompression from "vite-plugin-compression";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // 当前工作目录路径
 const root = process.cwd();
@@ -36,7 +37,15 @@ export default defineConfig(({ mode }) => {
       }),
       mockDevServerPlugin(),
       // 生产环境 gzip 压缩资源
-      viteCompression()
+      viteCompression(),
+      // 注入模板数据
+      createHtmlPlugin({
+        inject: {
+          data: {
+            ENABLE_ERUDA: env.VITE_ENABLE_ERUDA || "false"
+          }
+        }
+      })
     ],
     resolve: {
       alias: {
