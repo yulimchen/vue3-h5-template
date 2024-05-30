b3ac3c41ee2fb1bdf1f724cddd7bbd69c2aa4399
yulimchen <yulimchen@gmail.com>
2024-05-30 23:34:59 +0800

    chore: 调整视口适配转换为 vmin 单位

--- postcss.config.js
+++ postcss.config.js
@@ -4,8 +4,10 @@ export default {
     // 使用 cnjm-postcss-px-to-viewport 规避 postcss.plugin was deprecated 警告
     "cnjm-postcss-px-to-viewport": {
       viewportWidth: 375, // 根据设计稿设定
-      minPixelValue: 1, // 最小的转换数值
-      unitPrecision: 2 // 转化精度，转换后保留位数
+      unitPrecision: 4, // 转化精度，转换后保留位数
+      viewportUnit: "vmin", // 转换后的单位
+      fontViewportUnit: "vmin", // 字体单位
+      unitToConvert: "px" // 需要转换的单位
     },
     autoprefixer: {
       overrideBrowserslist: ["Android >= 4.0", "iOS >= 7"]
