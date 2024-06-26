d1c2611fc9a66fef5eef3d86a28ca32750ac8e7f
yulimchen <yulimchen@gmail.com>
2024-06-26 23:23:01 +0800

    feat: 集成 iconify 图标

---
+++ src/components/IIcon/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts" name="IIcon">
+import { Icon as IconifyIconComp } from "@iconify/vue";
+import type { IconifyIcon } from "@iconify/vue";
+
+const props = defineProps<{
+  icon: string | IconifyIcon;
+}>();
+</script>
+
+<template>
+  <IconifyIconComp :icon="props.icon" class="i-icon" />
+</template>
+
+<style scoped>
+.i-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/views/tools/index.vue
+++ src/views/tools/index.vue
@@ -3,6 +3,12 @@ import { getListApi, getListApiError } from "@/api/mock";
 import { reactive } from "vue";
 import { showFailToast, showSuccessToast } from "vant";
 import "vant/es/toast/style";
+import Fa6SolidAddressBook from "@iconify-icons/fa6-solid/address-book";
+import Fa6SolidAppleWhole from "@iconify-icons/fa6-solid/apple-whole";
+import Fa6SolidBaby from "@iconify-icons/fa6-solid/baby";
+import Fa6SolidBasketball from "@iconify-icons/fa6-solid/basketball";
+import Fa6SolidBurger from "@iconify-icons/fa6-solid/burger";
+import Fa6SolidChessKnight from "@iconify-icons/fa6-solid/chess-knight";
 
 const showList: string[] = reactive([]);
 
@@ -20,10 +26,33 @@ const handleErrorReq = () => {
     }
   );
 };
+
+const iconOnlineList = [
+  "material-symbols:admin-panel-settings-outline",
+  "jam:android",
+  "lucide:badge-check",
+  "pixelarticons:heart",
+  "fxemoji:alienmonster",
+  "meteocons:thunderstorms-day-overcast-fill"
+];
+const iconOfflineList = [
+  Fa6SolidAddressBook,
+  Fa6SolidAppleWhole,
+  Fa6SolidBaby,
+  Fa6SolidBasketball,
+  Fa6SolidBurger,
+  Fa6SolidChessKnight
+];
+// 获取所有本地 svg 图标文件名称
+const modules = import.meta.glob("../../icons/svg/*.svg", { eager: true });
+const svgIconLocalList = Object.keys(modules).map(key =>
+  key.replace("../../icons/svg/", "").replace(".svg", "")
+);
 </script>
 
 <template>
   <div class="tools-content pt-[20px] px-[12px]">
+    <!-- Mock -->
     <div class="pl-[12px] border-l-[3px] border-[color:#41b883] mb-[12px]">
       <h3 class="font-bold text-[18px] my-[4px]">Mock</h3>
     </div>
@@ -37,6 +66,44 @@ const handleErrorReq = () => {
       <p class="my-[14px] leading-[24px]">
         {{ showList }}
       </p>
+    </div>
+    <!-- Icon -->
+    <div
+      class="pl-[12px] border-l-[3px] border-[color:#41b883] mt-[24px] mb-[12px]"
+    >
+      <h3 class="font-bold text-[18px] my-[4px]">Iconify Icon</h3>
+    </div>
+    <!-- online iconify icon -->
+    <div>
+      <i-icon
+        v-for="item in iconOnlineList"
+        :key="item"
+        :icon="item"
+        class="inline-block text-[24px] mr-3"
+      />
+    </div>
+    <!-- offline iconify icon -->
+    <div class="mt-2">
+      <i-icon
+        v-for="(item, idx) in iconOfflineList"
+        :key="idx"
+        :icon="item"
+        class="inline-block text-[24px] mr-3"
+      />
+    </div>
+    <div
+      class="pl-[12px] border-l-[3px] border-[color:#41b883] mt-[24px] mb-[12px]"
+    >
+      <h3 class="font-bold text-[18px] my-[4px]">Svg Icon</h3>
+    </div>
+    <!-- local svg file icon -->
+    <div>
+      <svg-icon
+        v-for="item in svgIconLocalList"
+        :key="item"
+        :name="item"
+        class="inline-block text-[24px] mr-3"
+      />
     </div>
   </div>
 </template>
