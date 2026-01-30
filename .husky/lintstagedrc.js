c31cb2452c8292bc13dc5cde8b08093f6b15586d
yulimchen <yulimchen@gmail.com>
2026-01-30 18:40:30 +0800

    chore: `eslint` 规则改为 `@antfu/eslint-config`

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-  "{!(package)*.json}": ["prettier --write--parser json"],
-  "package.json": ["prettier --write"],
-  "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
-  "*.{vue,css,scss,postcss,less}": ["stylelint --fix", "prettier --write"],
-  "*.md": ["prettier --write"]
-};
+  '*.{js,jsx,ts,tsx,vue,json,md}': ['eslint --fix'],
+  '*.{css,scss,postcss,less}': ['stylelint --fix'],
+}
--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-/** @type {import("prettier").Config} */
-export default {
-  bracketSpacing: true,
-  singleQuote: false,
-  arrowParens: "avoid",
-  trailingComma: "none"
-};
