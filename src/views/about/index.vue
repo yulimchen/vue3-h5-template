9942def10dd7ff0715de01ecbbb70e049bb6e4a9
yulimchen <yulimchen@gmail.com>
2026-02-05 14:59:51 +0800

    chore: 版本打印

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import pkg from '../../../package.json'
+
 defineOptions({
   name: 'About',
 })
+
+const version = pkg.version
 </script>
 
 <template>
   <div>about</div>
+  <div class="text-sm text-gray-500">
+    Version: {{ version }}
+  </div>
 </template>
