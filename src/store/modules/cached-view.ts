ba751b69ca26641e4923c575eeb734db05e62248
yulimchen <yulimchen@gmail.com>
2026-02-05 16:11:23 +0800

    perf: `cached-view` 增加 `Set` 提升查找性能

--- src/store/modules/cached-view.ts
+++ src/store/modules/cached-view.ts
@@ -3,25 +3,29 @@ import { defineStore } from 'pinia'
 
 export const useCachedViewStore = defineStore('cached-view', {
   state: () => ({
-    // 缓存页面 keepAlive
+    cachedViewSet: new Set<string>(),
     cachedViewList: [] as string[],
   }),
   actions: {
     addCachedView(view: toRouteType) {
-      // 不重复添加
-      if (this.cachedViewList.includes(view.name as string))
+      const name = view.name as string
+      if (this.cachedViewSet.has(name)) // O(1)
         return
       if (!view?.meta?.noCache) {
-        this.cachedViewList.push(view.name as string)
+        this.cachedViewSet.add(name)
+        this.cachedViewList.push(name)
       }
     },
     delCachedView(view: toRouteType) {
-      const index = this.cachedViewList.indexOf(view.name as string)
+      const name = view.name as string
+      this.cachedViewSet.delete(name)
+      const index = this.cachedViewList.indexOf(name)
       if (index > -1) {
         this.cachedViewList.splice(index, 1)
       }
     },
     delAllCachedViews() {
+      this.cachedViewSet.clear()
       this.cachedViewList = []
     },
   },
